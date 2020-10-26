198b390ead99644facfc63c1428412779c413b9f
yangqian <743295483@qq.com>
2020-10-26 22:53:07 +0800

    [yangqian] feat: configuration properties read Map value.

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -3,6 +3,7 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
 import java.util.List;
+import java.util.Map;
 
 @ConfigurationProperties("mail")
 public class MailConfig {
@@ -11,6 +12,7 @@ public class MailConfig {
     private int port;
     private String from;
     private List<String> defaultRecipients;
+    private Map<String,Boolean> additionalHeaders;
 
     public String getHostname() {
         return hostname;
@@ -44,15 +46,26 @@ public class MailConfig {
         this.defaultRecipients = defaultRecipients;
     }
 
+    public Map<String, Boolean> getAdditionalHeaders() {
+        return additionalHeaders;
+    }
+
+    public void setAdditionalHeaders(Map<String, Boolean> additionalHeaders) {
+        this.additionalHeaders = additionalHeaders;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
-                ", defaultRecipients=['"
-                + defaultRecipients.get(0) + "', '"
-                + defaultRecipients.get(1) + "']" +
+                ", defaultRecipients=['" +
+                defaultRecipients.get(0) + "', '" +
+                defaultRecipients.get(1) + "']" +
+                ",additionalHeaders={" +
+                "redelivery=" + additionalHeaders.get("redelivery") +
+                "secure=" + additionalHeaders.get("secure") +
                 "}";
     }
 }
