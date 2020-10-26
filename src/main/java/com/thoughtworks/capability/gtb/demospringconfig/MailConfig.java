ecad66c18a53bd15b6e465fc88c695bcabbde817
yangqian <743295483@qq.com>
2020-10-26 22:47:50 +0800

    [yangqian] feat: configuration properties read List value.

--- src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
+++ src/main/java/com/thoughtworks/capability/gtb/demospringconfig/MailConfig.java
@@ -2,12 +2,15 @@ package com.thoughtworks.capability.gtb.demospringconfig;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
 
+import java.util.List;
+
 @ConfigurationProperties("mail")
 public class MailConfig {
 
     private String hostname;
     private int port;
     private String from;
+    private List<String> defaultRecipients;
 
     public String getHostname() {
         return hostname;
@@ -33,12 +36,23 @@ public class MailConfig {
         this.from = from;
     }
 
+    public List<String> getDefaultRecipients() {
+        return defaultRecipients;
+    }
+
+    public void setDefaultRecipients(List<String> defaultRecipients) {
+        this.defaultRecipients = defaultRecipients;
+    }
+
     @Override
     public String toString() {
         return "MailConfig{" +
                 "hostname='" + hostname + '\'' +
                 ", port=" + port +
                 ", from='" + from + '\'' +
-                '}';
+                ", defaultRecipients=['"
+                + defaultRecipients.get(0) + "', '"
+                + defaultRecipients.get(1) + "']" +
+                "}";
     }
 }
